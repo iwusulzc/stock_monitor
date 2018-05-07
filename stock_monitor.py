00863ea34dd207f59f151f140e8d46ca6083abbd
wsulzc <wsulzc@163.com>
2018-05-07 22:49:18 +0800

    update the struct

--- stock_monitor.py
+++ stock_monitor.py
@@ -5,16 +5,13 @@ import pandas as pd
 
 from abc import ABCMeta, abstractmethod 
 
-#b = Browser('chrome')
-#b.visit('http://data.eastmoney.com/zjlx/list.html')
-
 class BaseSpider(object):
 	__metaclass__ = ABCMeta
 	start_urls = []
 
 	@abstractmethod
-	def __init__(self, browser_driver='chrome'):
-		self.browser = Browser(browser_driver)
+	def __init__(self, browser_driver_name = 'chrome'):
+		self.browser = Browser(browser_driver_name)
 
 	@abstractmethod
 	def parse(self, browser):
@@ -34,72 +31,73 @@ class EastmoneySpider(Spider):
 	start_urls = ['http://data.eastmoney.com/zjlx/list.html']
 
 	def parse(self, browser):
-		items = self.today_up_info_parse(browser)
-		for data in items:
-			data = [data]
-			print(data)
-			h = pd.DataFrame(data)
-			h.to_csv('today_up_info.csv', index=False, header=False, encoding='gb2312', mode='a+')
+		for i in range(2):
+			items = self.today_up_info_parse(browser)
+			for data in items:
+				data = [data]
+				df = pd.DataFrame(data)
+				df.to_csv('today_up_info.csv', index=False, \
+					header = False, encoding = 'gb2312', mode = 'a+')
 
 	def today_up_info_parse(self, browser):
-		today_up = browser.find_by_text(u'今日涨跌')
-		if not today_up:
-			return 
-		today_up[1].click()
-		time.sleep(1)
+		tables = browser.find_by_xpath('//table[@id="dt_1"]')
+		today_up = tables.find_by_text(u'今日涨跌').last
+		if today_up:
+			today_up.click()
+			time.sleep(1)
 
-		tables = browser.find_by_xpath('//table[@id="dt_1"]/*/tr')
+			trs = browser.find_by_xpath('//table[@id="dt_1"]/*/tr')
 
-		head1 = tables[0].text.split()
-		head2 = tables[1].text.split()
-		head = []
-		head.extend(head1[1:3])
-		head.append(head2[0])
-		head.extend(head2[2:4])
-		head.extend(head2[5:7])
-		head.extend(head2[8:9])
-		head.append(head1[8])
+			head1 = trs[0].text.split()
+			head2 = trs[1].text.split()
+			head = []
+			head.extend(head1[1:3])
+			head.append(head2[0])
+			head.extend(head2[2:4])
+			head.extend(head2[5:7])
+			head.extend(head2[8:9])
+			head.append(head1[8])
 
-		for i in range(len(head)):
-			head[i]=re.sub(u'主力', '', head[i])
-		yield head
+			for i in range(len(head)):
+				head[i] = re.sub(u'主力', '', head[i])
+			yield head
 
-		today_up_stock = []
-		today_up_stock.append(head)
+			today_up_stock = []
+			today_up_stock.append(head)
 
-		stat = {}
+			stat = {}
 
-		for i in range(2, len(tables)):
-			content = []
-			c = tables[i].text.split()
+			for i in range(2, len(trs)):
+				content = []
+				c = trs[i].text.split()
 
-			# 今日涨跌值
-			up_value = float(re.sub('%', '', c[9]))
-			if (abs(up_value) < 5):
-				break
+				# 今日涨跌值
+				up_value = float(re.sub('%', '', c[9]))
+				if (abs(up_value) < 5):
+					break
 
-			content.extend(c[1:3])
-			content.append(c[7])
-			content.extend(c[9:11])
-			content.extend(c[12:14])
-			content.extend(c[15:17])
+				content.extend(c[1:3])
+				content.append(c[7])
+				content.extend(c[9:11])
+				content.extend(c[12:14])
+				content.extend(c[15:17])
 
-			if content[-1] in stat:
-				stat[content[-1]] += 1
-			else:
-				stat[content[-1]] = 1
+				if content[-1] in stat:
+					stat[content[-1]] += 1
+				else:
+					stat[content[-1]] = 1
 
-			yield content
+				yield content
 
-		st = sorted(stat.items(), key=lambda d: d[1], reverse=True)
-		head_len = len(head)
-		st_len = len(st)
+			st = sorted(stat.items(), key = lambda d: d[1], reverse = True)
+			head_len = len(head)
+			st_len = len(st)
 
-		for i in range(0, st_len, head_len):
-			content = []
-			for k, v in st[i : i + head_len]:
-				content.append('{}:{}'.format(k, v))
-			yield content
+			for i in range(0, st_len, head_len):
+				content = []
+				for k, v in st[i : i + head_len]:
+					content.append('{}:{}'.format(k, v))
+				yield content
 
 s = EastmoneySpider()
 s.start()
